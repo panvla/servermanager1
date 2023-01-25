631c1aecf40fa0a46f27a11401095017e7222644
panvla <pandvlado@gmail.com>
2023-01-25 14:07:54 +0100

    added slash before Downloads/png/...

--- servermanagerB/src/main/java/com/vladimirpandurov/servermanagerB/controller/ServerController.java
+++ servermanagerB/src/main/java/com/vladimirpandurov/servermanagerB/controller/ServerController.java
@@ -91,7 +91,7 @@ public class ServerController {
     }
     @GetMapping(path="/image/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/png/" + fileName));
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/png/" + fileName));
     }
 
 
