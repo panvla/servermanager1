5fc61f3ce943dd737259eae4487346406cedeabc
panvla <pandvlado@gmail.com>
2023-01-25 16:50:45 +0100

    bla

--- servermanagerF/src/app/service/server.service.ts
+++ servermanagerF/src/app/service/server.service.ts
@@ -13,7 +13,7 @@ import {Status} from '../enum/status.enum';
 export class ServerService {
   
 
-  private readonly apiUrl ='any';
+  private readonly apiUrl ='http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
