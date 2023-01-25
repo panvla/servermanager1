17f04bbe24bf7aa6db1c71c9dc36684394800983
panvla <pandvlado@gmail.com>
2023-01-25 23:18:38 +0100

    added some frontend.

--- servermanagerF/src/app/app.component.ts
+++ servermanagerF/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of, startWith } from 'rxjs';
 import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
@@ -13,6 +14,10 @@ import { ServerService } from './service/server.service';
 export class AppComponent implements OnInit{
 
   appState$: Observable<AppState<CustomResponse>>
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  filterStatus$ = this.filterSubject.asObservable();
 
   
   constructor(private serverService: ServerService){}
